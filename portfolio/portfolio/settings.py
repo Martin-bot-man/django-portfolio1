566c3b08ac957ade78d1873a7561918ee439c9fe
Martin-bot-man <martinwere14@gmail.com>
2025-11-03 03:03:26 +0300

    feat(static):installed whitenoise to handle static files

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uz#k&mlp+)_b!h#2z#1*did%u&_jehyf92-p&q77=h*qa_8qns'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
